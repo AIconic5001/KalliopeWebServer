d7db846b2bbdcd28685b2738dec6070c3ae5262a
huuquangnhatnguyen <nguye2hq@mail.uc.edu>
2025-03-17 19:39:35 -0400

    update readme for guideline

--- frontend/src/pages/Synopsis/CitationTimeline/CitationTimeline.tsx
+++ frontend/src/pages/Synopsis/CitationTimeline/CitationTimeline.tsx
@@ -1,7 +1,12 @@
 import './styles.scss';
 
 function CitationTimeline() {
-  return <div className='container'>This is the mock timeline section</div>;
+  return (
+    <div className='container'>
+      This is the mock timeline section
+      {/* Add any visualizations here */}
+    </div>
+  );
 }
 
 export default CitationTimeline;
